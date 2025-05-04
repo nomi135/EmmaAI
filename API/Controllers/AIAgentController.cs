3bf78dc3366413f53ca243774050d9dc354f401a
nomi135 <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>
2025-05-05 00:22:04 +0500

    .

--- API/Controllers/AIAgentController.cs
+++ API/Controllers/AIAgentController.cs
@@ -14,7 +14,7 @@ namespace API.Controllers
 {
     [Authorize]
     public class AIAgentController(IUserChatHistoryRepository chatHistoryRepository, IChatHandlerService chatHandlerService, ISpeechService speechService, Kernel kernel,
-                                   AzureOpenAIPromptExecutionSettings executionSettings, IMapper mapper, ILogger<AIAgentController> logger) : BaseApiController
+                                   AzureOpenAIPromptExecutionSettings executionSettings, IMapper mapper) : BaseApiController
     {
         [HttpPost]
         public async Task<ActionResult<AssistantMessageDto>> Chat([FromBody] UserMessageDto userMessage)
@@ -132,7 +132,6 @@ namespace API.Controllers
             var fileName = $"{sanitizedMessage}_{Guid.NewGuid()}_response.mp3";
             var responseAudioPath = Path.Combine(folderPath, fileName);
 
-            logger.LogInformation($"Audio will be saved to: {responseAudioPath}");
             Console.WriteLine($"Audio will be saved to: {responseAudioPath}");
 
             if (!Directory.Exists(folderPath))
